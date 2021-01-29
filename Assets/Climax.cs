bde285ade5d0c2771bd2c4a3badafcde2e1174a3
blaideSAE <1017007@student.sae.edu.au>
2021-01-30 02:26:25 +1100

    Update with a load of changes and optimisations.

---
+++ Assets/Climax.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class Climax : MonoBehaviour
+{
+    public float experienceLength;
+    public float climaxStartTime;
+    bool climaxStarted = false;
+    public UnityEvent BeginFadeAway;
+    float timer = 0;
+    bool TimeUp = false;
+    public float t;
+
+    public ParticleSystem[] particleSystems;
+
+
+   public ParticleSystemForceField climaxField;
+
+    float climaxSpinmax;
+    ParticleSystem.MinMaxCurve climaxGravity;
+    ParticleSystem.MinMaxCurve climaxSpin;
+    ParticleSystem.MinMaxCurve climaxSpinAttraction;
+    ParticleSystem.MinMaxCurve climaxY;
+
+
+    private void Start()
+    {
+        if (BeginFadeAway == null) 
+        {
+            BeginFadeAway = new UnityEvent();
+        }
+
+        climaxSpin = climaxField.rotationSpeed;
+        climaxGravity = climaxField.gravity;
+        climaxY = climaxField.directionY;
+        climaxSpinAttraction = climaxField.rotationAttraction;
+
+
+        climaxField.rotationSpeed = new ParticleSystem.MinMaxCurve(0);
+        climaxField.gravity = new ParticleSystem.MinMaxCurve(0);
+        climaxField.directionY = new ParticleSystem.MinMaxCurve(0);
+        climaxField.rotationAttraction = new ParticleSystem.MinMaxCurve(0);    //TODO add attraction lerping.
+        
+
+    }
+    private void Update()
+    {
+
+        if (timer + Time.deltaTime >= climaxStartTime * 60 && timer + Time.deltaTime < experienceLength * 60)
+        {
+            t = ((timer + Time.deltaTime) - climaxStartTime * 60) / (experienceLength * 60 - climaxStartTime * 60);
+
+
+            climaxField.rotationSpeed = new ParticleSystem.MinMaxCurve(Mathf.Lerp(0, climaxSpin.constant, t));
+            climaxField.gravity = new ParticleSystem.MinMaxCurve(Mathf.Lerp(0, climaxGravity.constant, t));
+            climaxField.directionY = new ParticleSystem.MinMaxCurve(Mathf.Lerp(0, climaxY.constant, t));
+            climaxField.rotationAttraction = new ParticleSystem.MinMaxCurve(Mathf.Lerp(0, climaxSpinAttraction.constant, t));
+
+
+            foreach (ParticleSystem p in particleSystems)
+            {
+                var em = p.emission;
+                em.enabled = false;
+
+            }
+
+        }
+        if (timer + Time.deltaTime < experienceLength * 60) 
+        {
+            timer += Time.deltaTime;
+        }
+        else if(!TimeUp)
+        {
+            TimeUp = true;
+            if (BeginFadeAway != null)
+            {
+                BeginFadeAway.Invoke();
+            }
+            
+        }
+
+    }
+}
---
+++ Assets/FadeAudio.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeAudio : MonoBehaviour
+{
+    public bool fadeInOnStart = false;    
+    bool fading = false;
+    bool fadeDir = false;
+    float currentVolPercent = 0;
+    float maxVol = 0;
+    public AudioSource source;
+    public float fadeSpeed;
+    private void Awake()
+    {
+        maxVol = source.volume;
+        if (fadeInOnStart) source.volume = 0;
+    }
+    private void Start()
+    {
+        
+        if(fadeInOnStart) FadeIn();
+    }
+
+    public void FadeIn() 
+    {
+        fading = true;
+        fadeDir = true;
+        currentVolPercent = 0;
+    }
+    public void FadeOut() 
+    {
+        fading = true;
+        fadeDir =false;
+        currentVolPercent = 1;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (fading) 
+        {
+            if (fadeDir)
+            {
+                if (currentVolPercent + Time.deltaTime * fadeSpeed < 1)
+                {
+                    currentVolPercent += Time.deltaTime * fadeSpeed;
+
+                    source.volume = Mathf.Lerp(0, maxVol, currentVolPercent);
+                }
+                else 
+                {
+                    currentVolPercent = 1;
+                    source.volume = Mathf.Lerp(0, maxVol, currentVolPercent);
+                    fading = false;
+                }
+
+
+            }
+            else 
+            {
+                if (currentVolPercent - Time.deltaTime * fadeSpeed > 0)
+                {
+                    currentVolPercent -= Time.deltaTime * fadeSpeed;
+
+                    source.volume = Mathf.Lerp(0, maxVol, currentVolPercent);
+                }
+                else
+                {
+                    currentVolPercent = 0;
+                    source.volume = Mathf.Lerp(0, maxVol, currentVolPercent);
+                    fading = false;
+                }
+
+
+            }
+        }   
+    }
+}
